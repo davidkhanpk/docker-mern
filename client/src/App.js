ad1a1afb94f72c0ae55287b6bf06c71115817ae0
David Khan <dskp526@gmail.com>
2020-03-14 17:03:34 +0500

    setup basic navbar and routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import { Container } from 'semantic-ui-react'
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      Docker React
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Menu, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+function MenuBar() {
+    const pathname = window.location.pathname;
+    const activeLink = pathname === '/' ? 'home' : pathname.substr(1); 
+    const [activeItem, setActiveItem] = useState(activeLink);
+    const handleItemClick = (e, { name }) => setActiveItem(name);
+
+//   render() {
+//     const { activeItem } = this.state
+
+    return (
+        <Menu pointing secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
+          
+          <Menu.Menu position='right'>
+            <Menu.Item name='login' active={activeItem === 'login'} onClick={handleItemClick} as={Link} to="/login" />
+            <Menu.Item name='register' active={activeItem === 'register'} onClick={handleItemClick} as={Link} to="/register" />
+          </Menu.Menu>
+        </Menu>
+    )
+//   }
+}
+
+export default MenuBar;
